ec749ff562ec8fd203030c86634a20f001c4a79e
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-05 21:42:14 +0200

    first get service version

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 import express = require('express');
+const bodyParser = require("body-parser");
+
+import { RoutesService } from './services/routes-service';
+
 
 const app: express.Application = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const routesService = new RoutesService();
+const getUrl = routesService.getRoute();
+console.log('URL -> ' + getUrl);
+
+app.get('/' + getUrl, function (req, res) {
+    const routes = routesService.getResponse();
+    console.log(routes);
+    res.json(routes);
+});
+
 app.get('/', function (req, res) {
-    res.send('CV Mock status OK');
+    res.send("Mock CM status OK");
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
