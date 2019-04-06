ca8196d7079368541aebb201a9a85acce5b8b382
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-06 13:14:40 +0200

    replace hardcode with constants

--- src/services/routes-service.ts
+++ src/services/routes-service.ts
@@ -2,19 +2,24 @@ import {IService} from "../interfaces/IService";
 import {IControllerInfo} from "../interfaces/IControllerInfo";
 
 const fs = require("fs");
+const RESOURCES_PATH = './resources/';
+const RESPONSE_FILE = 'response.json';
+const SERVICE_FILE = 'service.json';
+const ENCODE = 'utf8';
+
 
 export class RoutesService {
 
     getResponse(folder: string): Object {
-        return fs.readFileSync(`./resources/${folder}/response.json`, "utf8");
+        return fs.readFileSync(`${RESOURCES_PATH}${folder}/${RESPONSE_FILE}`, ENCODE);
     }
 
     getService(folder: string): IService {
-        return JSON.parse(fs.readFileSync(`./resources/${folder}/service.json`, "utf8"));
+        return JSON.parse(fs.readFileSync(`${RESOURCES_PATH}${folder}/${SERVICE_FILE}`, ENCODE));
     }
 
     getControllersInfo(): Array<IControllerInfo> {
-        const resourceFolder = fs.readdirSync('./resources/');
+        const resourceFolder = fs.readdirSync(RESOURCES_PATH);
         return resourceFolder.map((folder: any) => {
             return {
                 service: this.getService(folder),
