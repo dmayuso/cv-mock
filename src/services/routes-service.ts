45f63c8e119bc64d7a826e6435b19853b5146c18
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-06 13:04:20 +0200

    refactor service returning controller info

--- src/services/routes-service.ts
+++ src/services/routes-service.ts
@@ -1,14 +1,27 @@
+import {IService} from "../interfaces/IService";
+import {IControllerInfo} from "../interfaces/IControllerInfo";
+
 const fs = require("fs");
 
 export class RoutesService {
 
-    getResponse(): Object {
-        return fs.readFileSync("./resources/demo/response.json", "utf8");
+    getResponse(folder: string): Object {
+        return fs.readFileSync(`./resources/${folder}/response.json`, "utf8");
     }
 
-    getRoute(): string {
-        const service = JSON.parse(fs.readFileSync("./resources/demo/service.json", "utf8"));
-        return service.url;
+    getService(folder: string): IService {
+        return JSON.parse(fs.readFileSync(`./resources/${folder}/service.json`, "utf8"));
+    }
+
+    getControllersInfo(): Array<IControllerInfo> {
+        const resourceFolder = fs.readdirSync('./resources/');
+        return resourceFolder.map((folder: any) => {
+            return {
+                service: this.getService(folder),
+                response: this.getResponse(folder)
+            }
+
+        });
     }
 
 }
