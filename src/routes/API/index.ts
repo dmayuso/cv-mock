ab1241dac0f5e5fe9d1b30fda93cda63689dd461
j0nl1 <jonlitech@gmail.com>
2019-04-08 23:15:17 +0200

    Fix Method to generate fake Cards

--- src/routes/API/index.ts
+++ src/routes/API/index.ts
@@ -4,11 +4,19 @@ const router = express.Router();
 import { Request, Response } from "express";
 
 router.get("/demo", function(req: Request, res: Response) {
-    res.status(200).send(Array(100).fill(faker.helpers.createCard()));
+    res.status(200).send(
+      Array(100)
+        .fill(undefined)
+        .map(x => faker.helpers.createCard())
+    );
 });
 
 router.get("/demo/:id", function(req: Request, res: Response) {
-    res.status(200).send(Array(+req.params.id).fill(faker.helpers.createCard()));
+    res.status(200).send(
+      Array(+req.params.id)
+        .fill(undefined)
+        .map(x => faker.helpers.createCard())
+    );
 });
 
 module.exports = router;
